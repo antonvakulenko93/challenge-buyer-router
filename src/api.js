ace9a19b53d5de07ea6c5c35dec5f549e5d99561
anton_vakulenko <anton.vakulenko93@gmail.com>
2018-03-30 12:14:31 +0300

    Improve by using redis features

--- src/api.js
+++ src/api.js
@@ -1,75 +1,67 @@
 const sendJson = require('send-data/json')
-const promisify = require('util').promisify
+const { promisify } = require('util')
 
 const redisClient = require('./redis')('simpleClient')
 
 const body = promisify(require('body/json'))
 const asyncGet = promisify(redisClient.get.bind(redisClient))
-const getKeysAsync = promisify(redisClient.keys.bind(redisClient))
+const sinterAsync = promisify(redisClient.sinter.bind(redisClient))
+const mgetAsync = promisify(redisClient.mget.bind(redisClient))
+
+const multi = redisClient.multi()
 
 async function createBuyers (req, res, opt, cbErr) {
   try {
-    const data = await body(req, res)
-    redisClient.set(`${data.id}`, JSON.stringify(data))
+    const { id, offers } = await body(req, res)
+
+    multi.set(`buyer:${id}`, JSON.stringify({id, offers}))
+
+    offers.forEach((offer, i) => {
+      multi.set(`buyer:${id}${i}`, JSON.stringify(offer))
+
+      Object.keys(offer.criteria).forEach((key) =>
+        offer.criteria[key].forEach(criteria =>
+          multi.sadd(`${key}:${criteria}`, `buyer:${id}${i}`)))
+    })
     res.statusCode = 201
-    sendJson(req, res, data)
+    await multi.exec()
+    sendJson(req, res, {})
   } catch (e) {
     cbErr(e)
   }
 }
 async function getByersById (req, res, opt) {
-  const data = await asyncGet(`${opt.params.id}`)
+  const data = await asyncGet(`buyer:${opt.params.id}`)
   sendJson(req, res, JSON.parse(data))
 }
 
 async function routes (req, res, opt, errCb) {
-  const keys = await getKeysAsync('*')
-  const data = (await Promise.all(keys.map(key => asyncGet(key))))
-                  .map(JSON.parse)
-
   const { state, device } = opt.query
 
   const timestamp = new Date(Date.parse(opt.query.timestamp))
 
-  let resp = data.filter(({offers}) =>
-    offers.filter(({criteria, value}) =>
-        criteria.device.includes(device) && criteria.state.includes(state) &&
-        criteria.hour.includes(timestamp.getUTCHours()) &&
-        criteria.day.includes(timestamp.getUTCDay())
-    ).length
-  )
+  const matchKeys = {
+    device: `device:${device}`,
+    state: `state:${state}`,
+    hour: `hour:${timestamp.getUTCHours()}`,
+    day: `day:${timestamp.getUTCDay()}`
+  }
 
-  resp.sort((a, b) => {
-    a.offers = a.offers.sort((c, m) => {
-      if (c.value > m.value) {
+  const keys = await sinterAsync(Object.values(matchKeys))
+  const records = (await mgetAsync(keys))
+    .map(JSON.parse)
+    .sort((a, b) => {
+      if (a.value > b.value) {
         return -1
-      } else if (c.value < m.value) {
+      } else if (a.value < b.value) {
         return 1
       } else {
         return 0
       }
     })
 
-    b.offers = b.offers.sort((c, m) => {
-      if (c.value > m.value) {
-        return -1
-      } else if (c.value < m.value) {
-        return 1
-      } else {
-        return 0
-      }
-    })
-
-    if (a.offers[0].value > b.offers[0].value) {
-      return -1
-    } else if (a.offers[0].value < b.offers[0].value) {
-      return 1
-    } else {
-      return 0
-    }
-  })
   res.statusCode = 302
-  res.setHeader('location', resp[0].offers[0].location)
+  res.setHeader('location', records[0].location)
   sendJson(req, res)
 }
 
